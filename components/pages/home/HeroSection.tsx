be5e6d987e31c94440ce53aaf81b4e5a63a0ef21
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-01-12 20:40:47 +0200

    minorfix

--- components/pages/home/HeroSection.tsx
+++ components/pages/home/HeroSection.tsx
@@ -11,52 +11,53 @@ import Link from 'next/link'
 
 export const HeroSection = () => {
     return (
+        <Stack sx={{ backgroundColor: '#fafafa', width: '100%' }}>
 
-        <Stack sx={{
-            // pt: { xl: 2, md: 4, xs: 2 },
-            mt: '-100px',
-            position: 'relative',
-            px: { xl: 2, md: 4, xs: 2 },
-            width: '100%', maxWidth: 'xl', mx: 'auto',
-            backgroundColor: '#fafafa',
-            // backgroundColor: '#f5f5f5',
-            // backgroundColor: '#fff',
-            pb: 5
-        }}>
-            <Stack direction={{ xs: 'column', md: 'row' }} justifyContent={'space-between'} width={'100%'} spacing={4} >
-                <Stack width={{ xs: '100%', md: '50%' }}>
-                    <Stack spacing={4} height={'100%'} alignItems={'flex-start'} position={'relative'}
-                        justifyContent={'center'} alignContent={'flex-start'}>
-                        <Typography variant='h1' fontWeight={600}>
-                            <span style={{ color: theme.palette.secondary.main }}>Kiekvienus</span> namus parduodu kaip savus
-                        </Typography>
-                        <Typography variant='body1'>
-                            Jūsų atstovas visais nekilnojamojo turto klausimais. Susisiekime jūsų poreikių ir turto įvertinimui, ir kartu ieškokime geriausios sprendimo dėl jūsų nekilnojamojo turto.
-                        </Typography>
-                        <Button variant='contained' size='large' color='secondary' href='#priceInquiryForm'>
-                            Nemokamas turto vertinimas
-                        </Button>
-                        <Stack direction={'row'} spacing={{ md: 1, xs: .5 }} pt={.3} sx={{ alignItems: 'center' }}>
-                            <Link passHref href={`${process.env.NEXT_PUBLIC_ARUODAS_URL}`} aria-label="aruodas">
-                                <AruodasIcon />
-                            </Link>
-                            <Link passHref href={`${process.env.NEXT_PUBLIC_FACEBOOK_URL}`} aria-label="facebook">
-                                <FacebookIcon sx={{ color: theme.palette.primary.dark, fontSize: 22 }} />
-                            </Link>
-                            <Link passHref href={`${process.env.NEXT_PUBLIC_LINKEDIN_URL}`} aria-label="linkedin">
-                                <LinkedInIcon sx={{ color: theme.palette.primary.dark, fontSize: 22 }} />
-                            </Link>
+            <Stack sx={{
+                // pt: { xl: 2, md: 4, xs: 2 },
+                mt: '-80px',
+                position: 'relative',
+                px: { xl: 2, md: 4, xs: 2 },
+                width: '100%', maxWidth: 'xl', mx: 'auto',
+                backgroundColor: '#fafafa',
+                // backgroundColor: '#f5f5f5',
+                // backgroundColor: '#fff',
+                pb: 5
+            }}>
+                <Stack direction={{ xs: 'column', md: 'row' }} justifyContent={'space-between'} width={'100%'} spacing={4} >
+                    <Stack width={{ xs: '100%', md: '50%' }} pt={10}>
+                        <Stack spacing={4} height={'100%'} alignItems={'flex-start'} position={'relative'}
+                            justifyContent={'center'} alignContent={'flex-start'}>
+                            <Typography variant='h1' fontWeight={600}>
+                                <span style={{ color: theme.palette.secondary.main }}>Kiekvienus</span> namus parduodu kaip savus
+                            </Typography>
+                            <Typography variant='body1'>
+                                Jūsų atstovas visais nekilnojamojo turto klausimais. Susisiekime jūsų poreikių ir turto įvertinimui, ir kartu ieškokime geriausios sprendimo dėl jūsų nekilnojamojo turto.
+                            </Typography>
+                            <Button variant='contained' size='large' color='secondary' href='#priceInquiryForm'>
+                                Nemokamas turto vertinimas
+                            </Button>
+                            <Stack direction={'row'} spacing={{ md: 1, xs: .5 }} pt={.3} sx={{ alignItems: 'center' }}>
+                                <Link passHref href={`${process.env.NEXT_PUBLIC_ARUODAS_URL}`} aria-label="aruodas">
+                                    <AruodasIcon />
+                                </Link>
+                                <Link passHref href={`${process.env.NEXT_PUBLIC_FACEBOOK_URL}`} aria-label="facebook">
+                                    <FacebookIcon sx={{ color: theme.palette.primary.dark, fontSize: 22 }} />
+                                </Link>
+                                <Link passHref href={`${process.env.NEXT_PUBLIC_LINKEDIN_URL}`} aria-label="linkedin">
+                                    <LinkedInIcon sx={{ color: theme.palette.primary.dark, fontSize: 22 }} />
+                                </Link>
+                            </Stack>
+                            <Stack direction={'row'} spacing={{ md: 1, xs: .5 }} sx={{ pt: 4, position: 'absolute', bottom: 20, right: { xs: 0, md: 'auto' } }}>
+                                <SubdirectoryArrowRightIcon sx={{ color: theme.palette.secondary.main, fontSize: 18, }} />
+                                <Link passHref href={`/parduodami`}>
+                                    <Typography variant='body1' color={theme.palette.secondary.main} sx={{ ":hover": { textDecoration: 'underline' } }}>
+                                        {'Parduodami objektai'}
+                                    </Typography>
+                                </Link>
+                            </Stack>
                         </Stack>
-                        <Stack direction={'row'} spacing={{ md: 1, xs: .5 }} sx={{ pt: 4, position: 'absolute', bottom: 20, right: { xs: 0, md: 'auto' } }}>
-                            <SubdirectoryArrowRightIcon sx={{ color: theme.palette.secondary.main, fontSize: 18, }} />
-                            <Link passHref href={`/parduodami`}>
-                                <Typography variant='body1' color={theme.palette.secondary.main} sx={{ ":hover": { textDecoration: 'underline' } }}>
-                                    {'Parduodami objektai'}
-                                </Typography>
-                            </Link>
-                        </Stack>
-                    </Stack>
-                    {/* <Stack direction={'row'} spacing={{ md: 1, xs: .5 }} sx={{ alignItems: 'center' }}>
+                        {/* <Stack direction={'row'} spacing={{ md: 1, xs: .5 }} sx={{ alignItems: 'center' }}>
                <EmailIcon sx={{ color: "#fff", fontSize: 18 }} />
                <Link passHref href={`mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}`}>
                    <Typography variant='caption' color={'#fff'}>
@@ -64,14 +65,15 @@ export const HeroSection = () => {
                    </Typography>
                </Link>
            </Stack> */}
-                </Stack>
-                <Stack width={{ xs: '100%', md: '40%' }} position={'relative'} zIndex={2}>
-                    <Stack sx={{ position: 'relative', width: '100%', height: { xs: 500, md: 800 }, positon: 'relative', top: 150 }}>
-                        <Image priority alt={"Ernestas Noreikis NT nekilnojamas turtas brokeris"}
-                            layout='fill' objectFit='cover' objectPosition='top' src={'/assets/images/ernestas-noreikis-NT-brokeris.avif'} />
-                        {/* <Typography variant='h1' fontWeight={600}>
+                    </Stack>
+                    <Stack width={{ xs: '100%', md: '40%' }} position={'relative'} zIndex={2}>
+                        <Stack sx={{ position: 'relative', width: '100%', height: { xs: 500, md: 800 }, positon: 'relative', top: 150 }}>
+                            <Image priority alt={"Ernestas Noreikis NT nekilnojamas turtas brokeris"}
+                                layout='fill' objectFit='cover' objectPosition='top' src={'/assets/images/ernestas-noreikis-NT-brokeris.avif'} />
+                            {/* <Typography variant='h1' fontWeight={600}>
 Kiekvienus namus parduodu kaip savus
 </Typography> */}
+                        </Stack>
                     </Stack>
                 </Stack>
             </Stack>
