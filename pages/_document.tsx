134065fea3dbd20c7524f4eca68561f1bbc43007
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-01-16 10:07:34 +0200

    gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { GoogleAnalytics } from '@next/third-parties/google'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const gaId = process.env.NEXT_PUBLIC_GA_ID
@@ -7,18 +7,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', '${gaId}');
-                    `}
-        </Script>
+        {gaId && <GoogleAnalytics gaId={gaId} />}
       </Head>
       <body>
         <Main />
