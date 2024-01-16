86a4cb7c2389dcd6f9615c9f42ad147ebef0440a
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-01-16 09:41:32 +0200

    paslaugos tekstai

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
   images: {
-    domains: ['154.49.136.99:1340', '154.49.136.99', '154.49.136.99:3002'],
+    domains: ['www.api.noreikis.com', 'https://www.api.noreikis.com', '154.49.136.99:1340', '154.49.136.99', '154.49.136.99:3002'],
   },
   i18n,
 }
