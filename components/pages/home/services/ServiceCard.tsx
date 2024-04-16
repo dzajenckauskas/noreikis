be1918b0a8ec08a5e250eafd28617100c77f7044
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-04-16 19:27:33 +0300

    minor fix

--- components/pages/home/services/ServiceCard.tsx
+++ components/pages/home/services/ServiceCard.tsx
@@ -19,7 +19,7 @@ export const ServiceCard = ({ title, icon, text, minHeight }: Props) => {
             width={'100%'} minHeight={{ xs: 'none', sm: minHeight }} pb={8}
             position={'relative'}
             alignSelf={'flex-start'}
-            sx={{ p: { xs: 2, sm: 4, lg: 8 }, py: { xs: 4, sm: 4, lg: 8 }, ':hover': { boxShadow: 'rgba(17, 12, 46, 0.05) 0px 48px 100px 0px;' } }}>
+            sx={{ p: { xs: 2, sm: 4, lg: 8 }, py: { xs: 4, sm: 4, lg: 8 }, ':hover': { backgroundColor: '#ffffffB3', boxShadow: 'rgba(17, 12, 46, 0.05) 0px 48px 100px 0px;' } }}>
             {icon}
             <Typography color={theme.palette.primary.main} fontWeight={600} variant='h5' component={'h2'}>
                 {title}
